ca072b16e0654b6542eb8453c035295efcd9d22f
paik-dongjoo <113086103+paik-dongjoo@users.noreply.github.com>
2022-10-05 16:51:23 +0900

    [level 1] Title: 폰켓몬, Time: 1.64 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -2,11 +2,12 @@ import java.util.HashMap;
 
 class Solution {
     public int solution(int[] nums){
-        int answer = 0;
         HashMap<Integer, Integer> hm = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             hm.put(nums[i], nums[i]);
         }
+
+        int answer = 0;
         int arrSize = nums.length/2;
         int cntSize = hm.size();
 
