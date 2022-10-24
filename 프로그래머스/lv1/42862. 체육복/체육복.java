b1992538335ab84b3c3b9d2a3a1be8af0f3dd2c6
paik-dongjoo <113086103+paik-dongjoo@users.noreply.github.com>
2022-10-24 23:32:00 +0900

    Update 체육복.java

--- 프로그래머스/lv1/42862. 체육복/체육복.java
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -2,10 +2,10 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-            int answer = n - lost.length;
+        int answer = n - lost.length;
         Arrays.sort(lost);
         Arrays.sort(reserve);
- 
+
         for (int i = 0; i < lost.length; i++) {
             for (int j = 0; j < reserve.length; j++) {
                 if (lost[i] == reserve[j]) {
@@ -15,10 +15,10 @@ class Solution {
                 }
             }
         }
- 
-        for (int lostPerson : lost) {
+
+        for (int item : lost) {
             for (int i = 0; i < reserve.length; i++) {
-                if (reserve[i] == lostPerson + 1 || reserve[i] == lostPerson - 1) {
+                if (reserve[i] == item + 1 || reserve[i] == item - 1) {
                     answer++;
                     reserve[i] = -1;
                     break;
